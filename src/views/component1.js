a1e737078b4b24cfe822ffe73f60bad9d7f4811a
iosravandev <ravanmehdiyev.dev@gmail.com>
2024-11-25 17:14:03 +0400

    Commit from TeleportHQ

---
+++ src/views/component1.js
@@ -0,0 +1,352 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './component1.css'
+
+const Component1 = (props) => {
+  return (
+    <div className="component1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="component1-component1">
+        <div className="component1-frame20">
+          <div className="component1-frame22">
+            <div className="component1-logo"></div>
+            <img
+              alt="MARKETNGARANAPOSTERcopy1151"
+              src="/external/marketngaranapostercopy1151-pxoq-400h.png"
+              className="component1marketngaranaposte-rcopy1"
+            />
+            <div className="component1-slogan"></div>
+          </div>
+          <div className="component1-frame5">
+            <span className="component1-text10">
+              Marketing AIR Proqramı Haqqında
+            </span>
+            <span className="component1-text11">
+              <span className="component1-text12">
+                Azərbaycanın ilk praktiki marketinq təlimi
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text13">
+                olan
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text14">
+                &quot; Marketinq AIR&quot;
+              </span>
+              <span className="component1-text15">
+                {' '}
+                vasitəsilə siz, sahənin nəzəri və praktiki məlumatları ilə tanış
+                olacaqsınız.
+              </span>
+              <span>  </span>
+            </span>
+            <span className="component1-text17">
+              <span className="component1-text18">
+                &quot;Marketing AIR&quot;
+              </span>
+              <span className="component1-text19">
+                {' '}
+                Azərbaycanın ilk praktiki marketinq təlimidir.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text20">2013-ci</span>
+              <span className="component1-text21">
+                {' '}
+                ildən  keçirilən
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text22">
+                &quot;Marketing AIR&quot;
+              </span>
+              <span className="component1-text23">
+                {' '}
+                təlimi
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text24">
+                5
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text25">
+                moduldan ibarət olmaqla bir birindən peşəkar təlimçilər
+                tərəfindən tədris olunur.
+              </span>
+              <br></br>
+              <br></br>
+              <span className="component1-text28">
+                Təlimdə tədris ediləcək modullar:
+              </span>
+              <br></br>
+              <span className="component1-text30">1</span>
+              <span className="component1-text31">
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text32">-ci modul:</span>
+              <span className="component1-text33">
+                {' '}
+                Marketinq strategiyası və biznesin inkişafı 
+              </span>
+              <span className="component1-text34">2</span>
+              <span className="component1-text35">
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text36">-ci modul:</span>
+              <span className="component1-text37">
+                {' '}
+                Brendinq PR və marketinq kommunikasiyası 
+              </span>
+              <span className="component1-text38">3 -cü modul:</span>
+              <span className="component1-text39">
+                {' '}
+                Satış və ticari marketinq 
+              </span>
+              <span className="component1-text40">4</span>
+              <span className="component1-text41">
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text42">-cü modul:</span>
+              <span className="component1-text43"> Rəqəmsal Marketinq </span>
+              <span className="component1-text44">5</span>
+              <span className="component1-text45">
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text46">-ci modul:</span>
+              <span className="component1-text47"> CRM</span>
+              <br></br>
+              <span className="component1-text49">
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span className="component1-text51">5</span>
+              <span className="component1-text52">
+                {' '}
+                modul
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text53">3 ay</span>
+              <span className="component1-text54">
+                {' '}
+                ərzində tədris olunur qiymət
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="component1-text55">600</span>
+              <span> manat təşkil edir</span>
+              <br></br>
+            </span>
+            <button className="component1-buttonsetstar1">
+              <span className="component1-text58">Təlimə Qoşul</span>
+            </button>
+          </div>
+          <span className="component1-text59">Təlimçilərimiz kimlərdir?</span>
+          <span className="component1-text60">
+            <span className="component1-text61">
+              Rafiq Hunaltay
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="component1-text62">
+              “Facemark” və “Fil Agency” təsisçisi
+            </span>
+            <br></br>
+            <span className="component1-text64">Sənan Orucov,</span>
+            <span className="component1-text65">
+              {' '}
+              Peşəkar Marketoloq və Biznes Konsultant
+            </span>
+            <span className="component1-text66">
+               Elşən Məhərov,
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="component1-text67">
+              Avrora MMC Layihə Direktoru
+            </span>
+            <br></br>
+            <span className="component1-text69">Tural Əlizadə,</span>
+            <span className="component1-text70">
+              {' '}
+              “MKT İstehsalat Kommersiya” MMC-də Kommersiya Direktoru
+            </span>
+            <br></br>
+            <span className="component1-text72">
+              Fərid Əfəndi,
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="component1-text73">
+              Evido Azerbaijan şirkətinin rəhbəri
+            </span>
+            <span className="component1-text74"> Səfər Aslanov,</span>
+            <span className="component1-text75">
+              {' '}
+              “Aztelekom” MMC və “Bakı Telefon Rabitəsi&quot; MMC-nin Korporativ
+              Əlaqələr sektorunun rəhbəri
+            </span>
+            <br></br>
+            <span className="component1-text77">Mahal Abayeva,</span>
+            <span className="component1-text78"> Marketinq Ekspert</span>
+            <br></br>
+            <span className="component1-text80">Şahnaz Əhmədova,</span>
+            <span className="component1-text81"> Sosial Media Mütəxəssisi</span>
+            <br></br>
+            <span className="component1-text83">Nofəl Əliyev,</span>
+            <span>
+              {' '}
+              Vətəndaşlara Xidmət və Sosial İnnovasiyalar üzrə Dövlət
+              Agentliyinin &quot;İnnovasiyalar Mərkəzi&quot;ndə İctimaiyyətlə
+              əlaqələr üzrə baş mütəxəssis
+            </span>
+            <br></br>
+            <span>  </span>
+            <br></br>
+            <span>  </span>
+          </span>
+          <div className="component1-frame23"></div>
+          <button className="component1-buttonsetstar2">
+            <span className="component1-text89">Digər Təlimlərlə Tanış Ol</span>
+          </button>
+          <div className="component1-group41">
+            <img
+              alt="Layer2163"
+              src="/external/layer2163-c74i.svg"
+              className="component1-layer21"
+            />
+          </div>
+          <div className="component1-frame24">
+            <div className="component1-group5">
+              <div className="component1-group8">
+                <img
+                  alt="Instagram184"
+                  src="/external/instagram184-mdl-200w.png"
+                  className="component1-instagram"
+                />
+                <img
+                  alt="Facebook185"
+                  src="/external/facebook185-i4e-200w.png"
+                  className="component1-facebook"
+                />
+                <img
+                  alt="LinkedIn186"
+                  src="/external/linkedin186-wvh58-200w.png"
+                  className="component1-linked-in"
+                />
+                <img
+                  alt="WhatsApp187"
+                  src="/external/whatsapp187-dzfi-200w.png"
+                  className="component1-whats-app"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="component1-frame26">
+            <div className="component1-group7">
+              <span className="component1-text90">info@fmrtrainigs.az</span>
+              <img
+                alt="Letter191"
+                src="/external/letter191-nrhr-200h.png"
+                className="component1-letter"
+              />
+            </div>
+          </div>
+          <div className="component1-frame25">
+            <div className="component1-group6">
+              <span className="component1-text91">+994 50 235 81 43</span>
+              <img
+                alt="Phone195"
+                src="/external/phone195-upvs-200w.png"
+                className="component1-phone"
+              />
+            </div>
+          </div>
+          <div className="component1-frame27">
+            <img
+              alt="logosvg21197"
+              src="/external/logosvg21197-jyd8.svg"
+              className="component1-logosvg21"
+            />
+            <span className="component1-text92">Produced by</span>
+          </div>
+        </div>
+        <img
+          alt="MARKETNGARANAPOSTERcopyp11109"
+          src="/external/marketngaranapostercopyp11109-92et-400h.png"
+          className="component1marketngaranaposte-rcopyp1"
+        />
+        <div className="component1-group42">
+          <img
+            alt="Layer21111"
+            src="/external/layer21111-19kk.svg"
+            className="component1-layer22"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Component1
